a5d137293a35ff9c8ade44b938b1dcd2aa8ca519
szymon <szymon.kicinski@interia.eu>
2018-08-01 21:39:25 +0200

    Initialize show index 1# step - empty template

--- src/main/java/huru/springframework/spring5webapp/repositories/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package huru.springframework.spring5webapp.repositories;
-
-import huru.springframework.spring5webapp.model.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-
-}
--- src/main/java/huru/springframework/spring5webapp/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package huru.springframework.spring5webapp.repositories;
-
-import huru.springframework.spring5webapp.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository <Book, Long> {
-}
